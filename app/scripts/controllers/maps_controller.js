9ae1cff3ad45d8a62da6dedc0931c98d76238375
Ryan Y. <ryayak1460@users.noreply.github.com>
2014-06-12 15:31:59 -0400

    Linted this controller.

--- app/scripts/controllers/maps_controller.js
+++ app/scripts/controllers/maps_controller.js
@@ -17,25 +17,39 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 Norfolkart.MapController = Ember.ObjectController.extend({
+    // Implement your controller here.
     actions: {
-	findMe: function () {
-	    this.findMe();
-	}
-    }, findMe: function () {
-	window.navigator.geolocation.getCurrentPosition(this.centerMap, this.positionError);
-    }, centerMap: function (position) {
-	alert(position.coords.latitude);
-	alert(position.coords.longitude);
-	this.latitude = position.coords.latitude;
-	this.longitude = position.coords.longitude;
-    }, positionError: function (error) {
-	alert(
-	    error.code === 1 ? 'Permission denied.' :
-	    error.code === 2 ? 'Position unavailable.' :
-	    error.code === 3 ? 'Timeout.' : 'Unknown error.');
-    }, hasMaps: true
-    , latitude: 36.84765224454971
-    , longitude: -76.2922677397728
-  // Implement your controller here.
+        findMe: function () {
+            'use strict';
+            this.findMe();
+        }
+    },
+    findMe: function () {
+        'use strict';
+        window.navigator.geolocation.getCurrentPosition(this.centerMap, this.positionError);
+    },
+    centerMap: function (position) {
+        'use strict';
+        alert(position.coords.latitude);
+        alert(position.coords.longitude);
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+    },
+    positionError: function (error) {
+        'use strict';
+        alert(
+            error.code === 1 ?
+                    'Permission denied.' :
+                    error.code === 2 ?
+                            'Position unavailable.' :
+                            error.code === 3 ?
+                                    'Timeout.' :
+                                    'Unknown error.'
+        );
+    },
+    hasMaps: true,
+    latitude: 36.84765224454971,
+    longitude: -76.2922677397728
 });
+
 
