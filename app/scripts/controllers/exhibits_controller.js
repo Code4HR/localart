93e614f87ae53a38881bdd02a25b8693ca7177bc
Ryan Y. <ryayak1460@users.noreply.github.com>
2014-06-12 17:26:59 -0400

    Linted and documented the exhibits controller stub

--- app/scripts/controllers/exhibits_controller.js
+++ app/scripts/controllers/exhibits_controller.js
@@ -16,7 +16,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/** The model for the exhibits controller.
+  *
+  * @class ExhibitsController
+  * @constructor */
 Norfolkart.ExhibitsController = Ember.ObjectController.extend({
   // Implement your controller here.
 });
-
