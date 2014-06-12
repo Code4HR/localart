6417675476599f7d9a2ee29a817d7bbb7f577853
Ryan Y. <ryayak1460@users.noreply.github.com>
2014-06-12 15:57:17 -0400

    Documented with YUIDoc syntax.

--- app/scripts/controllers/maps_controller.js
+++ app/scripts/controllers/maps_controller.js
@@ -16,14 +16,27 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/** The model for the map controller.
+  *
+  * @class MapController
+  * @constructor */
 Norfolkart.MapController = Ember.ObjectController.extend({
     // Implement your controller here.
     actions: {
+        /** Computation, public access to the geolocation function. 
+          *
+          * @public
+          * @method findMe */
         findMe: function () {
             'use strict';
             this.findMe();
         }
     },
+
+    /** Computation, gets the current geolocation position of the user. 
+      *
+      * @method findMe */
     findMe: function () {
         'use strict';
         window.navigator.geolocation.getCurrentPosition(
@@ -31,13 +44,28 @@ Norfolkart.MapController = Ember.ObjectController.extend({
             this.positionError
         );
     },
+
+    /** Computation, centres the map on the given geolocation position.
+      *
+      * @method centerMap
+      *
+      * @param {Position} position A Position value to centre the map upon. */
     centerMap: function (position) {
         'use strict';
+
+        // Currently, just shows the coordinates.
         alert(position.coords.latitude);
         alert(position.coords.longitude);
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
     },
+
+    /** Computation, alerts the user that a geolocation error occurred.
+      *
+      * @method positionError
+      *
+      * @param {PositionError} error A PositionError value representing the 
+      *     error. */
     positionError: function (error) {
         'use strict';
         alert(
@@ -50,8 +78,27 @@ Norfolkart.MapController = Ember.ObjectController.extend({
                                     'Unknown error.'
         );
     },
+
+    /** Predicate constant, proves this controller has maps.
+      *
+      * @deprecated
+      * @property hasMaps
+      * @type Boolean
+      * @default true */
     hasMaps: true,
+
+    /** Represents the centre latitude.
+      *
+      * @property latitude
+      * @type Number
+      * @default 36.84765224454971 */
     latitude: 36.84765224454971,
+
+    /** Represents the centre longitude.
+      *
+      * @property longitude
+      * @type Number
+      * @default -76.2922677397728 */
     longitude: -76.2922677397728
 });
 
