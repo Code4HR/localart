c306c25067246516c51b43cf4796c4f89eb24c69
Ryan Y <ryayak1460@protingumas.com>
2015-04-14 13:05:59 -0400

    Correct GPL headers for acceptance testing support code.

--- features/support/world.js
+++ features/support/world.js
@@ -1,5 +1,5 @@
 /*
-   Local Art API - Supports iArtNorfolk apps.
+   An HTML5 mobile app for navigating & browsing locations of public art,
    Copyright (C) 2015 Code for Hampton Roads Volunteers (http://code4hr.org)
 
    This program is free software: you can redistribute it and/or modify
